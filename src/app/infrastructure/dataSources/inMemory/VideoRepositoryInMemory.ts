cd584de42b6605aa01e3525bd61653a2a5f94b17
rolyalc <rolalcedu@gmail.com>
2025-11-06 01:04:52 +0100

    add(VideoRepositoryInMemory) implementación concreta y "en memoria" del contrato "IVideoRepo"

---
+++ src/app/infrastructure/dataSources/inMemory/VideoRepositoryInMemory.ts
@@ -0,0 +1,29 @@
+import { IVideoRepository } from "../../../domain/repositories/IVideoRepository";
+import { Video } from "../../../domain/entities/Video";
+import { VideoMapper } from "../../mappers/VideoMapper";
+import { VideoRecord } from "./models/VideoRecord";
+import { videos } from "../../../data/dades";
+
+export class VideoRepositoryInMemory implements IVideoRepository {
+  private records: VideoRecord[] = videos.map(v => ({
+    ...v,
+    createdAt: new Date().toISOString()
+  }));
+
+  async create(video: Video): Promise<Video> {
+    const newRecord: VideoRecord = VideoMapper.toRecord(video);
+
+    this.records.push(newRecord);
+
+    return video;
+  }
+
+  async findById(id: string): Promise<Video | null> {
+    const record = this.records.find(v => v.id === id);
+    return record ? VideoMapper.toDomain(record) : null;
+  }
+
+  async findAll(): Promise<Video[]> {
+    return this.records.map(VideoMapper.toDomain);
+  }
+}
