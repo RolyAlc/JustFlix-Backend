ebaaf328133554d25641c344c65ccebff6042357
rolyalc <rolalcedu@gmail.com>
2025-11-06 01:47:32 +0100

    add(video.routes) Añadir rutas

---
+++ src/app/http/routes/video.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { CreateVideoUseCase } from "../../domain/usecases/video/CreateVideoUseCase";
+import { GetVideoByIdUseCase } from "../../domain/usecases/video/GetVideoByIdUseCase";
+import { GetVideosUseCase } from "../../domain/usecases/video/GetVideosUseCase";
+import { VideoRepositoryInMemory } from "../../infrastructure/dataSources/inMemory/VideoRepositoryInMemory";
+import { VideoController } from "../controllers/video.controller";
+
+// Creem una implementació del repositori
+const repo = new VideoRepositoryInMemory();
+
+/**
+ * Creem el Controlador per als videos, proporcionant-li
+ * instàncies dels casos d'ús, que al seu temps hem inicialitzat
+ * amb el repositori (injecció de dependències)
+ */
+const controller = new VideoController(
+  new CreateVideoUseCase(repo),
+  new GetVideosUseCase(repo),
+  new GetVideoByIdUseCase(repo)
+);
+
+const VideoRouter = Router();
+// Definim les rutes dins aquest router i enllacem amb les funcions corresponents del controlador.
+VideoRouter.post("/", controller.create);
+VideoRouter.get("/", controller.getAll);
+VideoRouter.get("/:id", controller.getById);
+
+// Exportem el router
+export default VideoRouter;
