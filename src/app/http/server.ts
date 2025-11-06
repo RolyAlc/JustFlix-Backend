f3897798860b29a9fe655fe94f4b946b7bb37d24
rolyalc <rolalcedu@gmail.com>
2025-11-06 02:02:11 +0100

    fix(server.ts) error en validar la 404. quitado "*" por incompatibilidad de libreria express path-to-regexp

--- src/app/http/server.ts
+++ src/app/http/server.ts
@@ -1,22 +1,14 @@
 import express, { Request, Response, NextFunction } from "express";
-
-// Importem els routers de videos
 import VideoRouter from "./routes/video.routes";
 
 export function buildServer() {
-  // Creem l'aplicació Express
   const app = express();
 
-  // Utilitzem el middleware express.json per entende 
-  // els JSON al cos de les peticions
   app.use(express.json());
 
-
-  // Configurem l'aplicació per usar els routers/miniaplicacions
-  // Tindrem un router (miniaplicació) para videos
   app.use("/api/videos", VideoRouter);
 
-  app.use("*", function (req: Request, res: Response, next: NextFunction) {
+  app.use(function (req: Request, res: Response, next: NextFunction) {
     res.status(404).json({ message: "Resource not found" });
   });
 
