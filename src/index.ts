ce5179c8d8272edf95b0244ad4572ea35c215c0d
rolyalc <rolalcedu@gmail.com>
2025-11-06 02:03:51 +0100

    mod(index.ts) quitar instancia de express. conflicto con fichero server.ts. utilizar la instancia ya creada

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import config from './config';
-import express from 'express';
+import { buildServer } from './app/http/server';
 
-const app = express();
+const app = buildServer();
 const port = config.port;
-
-app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Servidor activo.");
