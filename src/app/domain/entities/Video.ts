50d8a70b0bd6ee322f9102e336370142761e86b0
rolyalc <rolalcedu@gmail.com>
2025-11-12 21:29:23 +0100

    mod(Viideo.ts): doc y añadido nueva propiedad videoUrl

--- src/app/domain/entities/Video.ts
+++ src/app/domain/entities/Video.ts
@@ -1,7 +1,13 @@
+/**
+ * Representa un objeto de vídeo con sus propiedades.
+ * @interface Video
+ */
+
 export interface Video {
     id: string;
     topic: string;
     description: string;
     duration: number;
     thumbnail: string;
+    videoUrl: string;
 }
