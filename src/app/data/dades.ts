effbe238ecde89fc498001226443a38311314820
rolyalc <rolalcedu@gmail.com>
2025-11-18 00:07:11 +0100

    mod(dades.ts): añadir info de video_01-03 (reales)

--- src/app/data/dades.ts
+++ src/app/data/dades.ts
@@ -1,75 +1,36 @@
 let videos = [
   {
-    "id": "video_01",
+    "id": "video_00",
     "topic": "Flutter",
     "description": "Demo",
     "duration": 300,
     "thumbnail": "assets/videos/demo/thumbnail/thumbnail_demo.png",
     "videoUrl": "/assets/videos/demo/hls/index.m3u8"
   },
-  /* {
+  {
+    "id": "video_01",
+    "topic": "Flutter",
+    "description": "video_01",
+    "duration": 310,
+    "thumbnail": "assets/videos/video_01/thumbnail/thumbnail_video_01.png",
+    "videoUrl": "/assets/videos/video_01/hls/index.m3u8"
+  },
+    {
     "id": "video_02",
-    "topic": "Instalación y configuración del entorno",
-    "description": "Cómo instalar Flutter, Dart SDK y configurar VS Code o Android Studio.",
-    "duration": 420,
-    "thumbnail": "http://localhost:3002/assets/thumbnails/flutter.png"
+    "topic": "Flutter",
+    "description": "video_02",
+    "duration": 320,
+    "thumbnail": "assets/videos/video_02/thumbnail/thumbnail_video_02.png",
+    "videoUrl": "/assets/videos/video_02/hls/index.m3u8"
   },
-  {
+    {
     "id": "video_03",
-    "topic": "Estructura de un proyecto Flutter",
-    "description": "Explicación de carpetas, archivos y cómo funciona la app principal (main.dart).",
-    "duration": 360,
-    "thumbnail": "http://localhost:3002/assets/thumbnails/flutter.png"
+    "topic": "Flutter",
+    "description": "video_03",
+    "duration": 330,
+    "thumbnail": "assets/videos/video_03/thumbnail/thumbnail_video_03.png",
+    "videoUrl": "/assets/videos/video_03/hls/index.m3u8"
   },
-  {
-    "id": "video_04",
-    "topic": "Widgets básicos en Flutter",
-    "description": "Aprender sobre widgets como Text, Column, Row, Container y Scaffold.",
-    "duration": 480,
-    "thumbnail": "http://localhost:3002/assets/thumbnails/flutter.png"
-  },
-  {
-    "id": "video_05",
-    "topic": "Navegación entre pantallas",
-    "description": "Cómo usar Navigator y rutas para moverte entre diferentes pantallas.",
-    "duration": 540,
-    "thumbnail": "http://localhost:3002/assets/thumbnails/flutter.png"
-  },
-  {
-    "id": "video_06",
-    "topic": "Stateful y Stateless Widgets",
-    "description": "Comprende la diferencia entre widgets con y sin estado, y cuándo usarlos.",
-    "duration": 480,
-    "thumbnail": "http://localhost:3002/assets/thumbnails/flutter.png"
-  },
-  {
-    "id": "video_07",
-    "topic": "Manejo del estado en Flutter",
-    "description": "Introducción a las formas de manejar estado: setState, Provider y Riverpod.",
-    "duration": 600,
-    "thumbnail": "http://localhost:3002/assets/thumbnails/flutter.png"
-  },
-  {
-    "id": "video_08",
-    "topic": "Diseño responsivo",
-    "description": "Cómo adaptar tu aplicación a diferentes tamaños de pantalla y orientación.",
-    "duration": 480,
-    "thumbnail": "http://localhost:3002/assets/thumbnails/flutter.png"
-  },
-  {
-    "id": "video_09",
-    "topic": "Consumo de APIs REST",
-    "description": "Aprende a realizar peticiones HTTP y mostrar datos en tu app.",
-    "duration": 660,
-    "thumbnail": "http://localhost:3002/assets/thumbnails/flutter.png"
-  },
-  {
-    "id": "video_10",
-    "topic": "Persistencia de datos",
-    "description": "Guarda información localmente usando SharedPreferences y SQLite.",
-    "duration": 720,
-    "thumbnail": "http://localhost:3002/assets/thumbnails/flutter.png"
-  } */
 ];
 
 export { videos };
