5743d2c933e37c70ff9b2ce18d37cd31af6f2a97
rolyalc <rolalcedu@gmail.com>
2025-11-18 19:24:36 +0100

    doc, mod(config.ts); documentar y añadiro validador de puerto

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+/**
+ * Configuración principal de la aplicación.
+ * Carga valores desde variables de entorno y aplica valores por defecto, en coso contrario.
+ */
 const config = {
-    port: parseInt(process.env.PORT || '3000', 10),
+    port: Number(process.env.PORT) || 3000,
 };
+
+// Valida los valores del puerto para que la aplicación no arranque con datos inválidos. Debido al [Number].
+if (isNaN(config.port)) {
+    throw new Error("La variable de entorno PORT debe de ser un número válido.")
+}
 
 export default config;
