c8d203b7b65e64d8473e3dbf69842c1165e532e8
rolyalc <rolalcedu@gmail.com>
2025-11-25 11:44:52 +0100

    doc(DOCUMENTACION) de varios ficheros

--- src/app/infrastructure/dataSources/inMemory/VideoRepositoryInMemory.ts
+++ src/app/infrastructure/dataSources/inMemory/VideoRepositoryInMemory.ts
@@ -4,12 +4,30 @@ import { VideoMapper } from "../../mappers/VideoMapper";
 import { VideoRecord } from "./models/VideoRecord";
 import { videos } from "../../../data/dades";
 
+/**
+ * Repositorio en memoria para la gestión de videos.
+ * Implementa la intefaz IVideoRepository para desacoplar
+ * la lógica de negocio de la persistencia de datos.
+ * @class VideoRepositoryInMemory
+ * @implements {IVideoRepository}
+ */
 export class VideoRepositoryInMemory implements IVideoRepository {
+
+  /**
+   * @private
+   * @type {VideoRecord[]}
+   * Almacena los registro de video sen memoria.
+   */
   private records: VideoRecord[] = videos.map(v => ({
     ...v,
     createdAt: new Date().toISOString()
   }));
 
+  /**
+   * 
+   * @param video Crea un nuevo video y lo almacen en el repositorio en memoria.
+   * @returns {Promise<Video>} Una promesa que resuelve con el Video creado.
+   */
   async create(video: Video): Promise<Video> {
     const newRecord: VideoRecord = VideoMapper.toRecord(video);
 
@@ -18,18 +36,32 @@ export class VideoRepositoryInMemory implements IVideoRepository {
     return video;
   }
 
+  /**
+   * Busca un video po su ID en el repositorio en memoria.
+   * @param {String} id - El ID del video a buscar.
+   * @returns {Promise<Video | null>} Una promesa que resuelve con el Video encontrado o null si no existe.
+   */
   async findById(id: string): Promise<Video | null> {
     const record = this.records.find(v => v.id === id);
     return record ? VideoMapper.toDomain(record) : null;
   }
 
+  /**
+   * Devuelve todos los videos del repositorio en memoria
+   * @returns {Promise<Video[]>} Una promesa que resuelve con una lista de todos los videos.
+   */
   async findAll(): Promise<Video[]> {
     return this.records.map(VideoMapper.toDomain);
   }
 
+  /**
+   * Busca videos por tema (case) en le repositorio en memoria.
+   * @param topic - El tema por el cual buscar videos.
+   * @returns {Promise<Video[]>} Una promesa que resuelve con una lista de videos que coinciden con el tema.
+   */
   async findByTopic(topic: string): Promise<Video[]> {
     const records = this.records.filter(record => record.topic.toLowerCase() === topic.toLowerCase()
-  );
-  return records.map(VideoMapper.toDomain);
+    );
+    return records.map(VideoMapper.toDomain);
   }
 }
--- src/app/infrastructure/mappers/VideoMapper.ts
+++ src/app/infrastructure/mappers/VideoMapper.ts
@@ -3,8 +3,19 @@
 import { Video } from "../../domain/entities/Video";
 import { VideoRecord } from "../dataSources/inMemory/models/VideoRecord";
 
+/**
+ * @class VideoMapper
+ * Clase estática para mapear entre la entidad de dominio `Video y le modelo de persistencia `VideoRecords`.
+ * Encargada de convertir la representación de la capa de persistencia a la capa de dominio y viceversa.
+ */
 export class VideoMapper {
-    static toDomain( record: VideoRecord ): Video {
+    /**
+     * Convierte un `VideoRecord` (q es el modelo de persistencia) a una entidad `Video` (dominio).
+     * Excluye lso campos que son especificos de la persistencia y no forma parte del dominio, como `createAt`
+     * @param {VideoRecord} record - El registro de video de la capa de persistencia. 
+     * @returns {Video} La entidad de cominio Video.
+     */
+    static toDomain(record: VideoRecord): Video {
         // Desestructuración de objetos.
         const { createdAt, ...domainData } = record;
         // Devolver solamente los datos q se correspondan con la entidad de dominio.
@@ -13,8 +24,10 @@ export class VideoMapper {
 
     /**
      * Convierte una entidad de dominio (Video) a un modelo de persistencia (VideoRecord).
-     * Su trabajo es añadir los campos necesarios para la persistencia que no existen
+     * Su trabajo es añadir los campos necesarios para la persistencia que no existen, como `createAt`
      * en el dominio.
+     * @param {Video} video - La entidad de jdominio Video.
+     * @returns {VideoRecord} El modelo de persistencia Video Record.
      */
     static toRecord(video: Video): VideoRecord {
         return {
