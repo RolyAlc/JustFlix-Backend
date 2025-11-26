f804a26b81326629ffa624f78864fce4278fe8ec
rolyalc <rolalcedu@gmail.com>
2025-11-26 17:50:45 +0100

    mod(dades.ts): añadido nombre del video en id

--- src/app/data/dades.ts
+++ src/app/data/dades.ts
@@ -1,32 +1,32 @@
 let videos = [
-  {
+/*   {
     "id": "video_00",
     "topic": "Flutter",
     "description": "Demo",
     "duration": 300,
     "thumbnail": "/assets/videos/demo/thumbnail/thumbnail_demo.png",
     "videoUrl": "/assets/videos/demo/hls/index.m3u8"
-  },
+  }, */
   {
-    "id": "video_01",
-    "topic": "Flutter",
-    "description": "video_01",
+    "id": "Hardware y getión de recursos de los smartphones",
+    "topic": "PSP",
+    "description": "Hardware y getión de recursos de los smartphones",
     "duration": 310,
     "thumbnail": "/assets/videos/video_01/thumbnail/thumbnail_video_01.png",
     "videoUrl": "/assets/videos/video_01/hls/index.m3u8"
   },
     {
-    "id": "video_02",
-    "topic": "Flutter",
-    "description": "video_02",
+    "id": "Desarrollo móvil: tecnologías y arquitectura android",
+    "topic": "PSP",
+    "description": "Desarrollo móvil: tecnologías y arquitectura android",
     "duration": 320,
     "thumbnail": "/assets/videos/video_02/thumbnail/thumbnail_video_02.png",
     "videoUrl": "/assets/videos/video_02/hls/index.m3u8"
   },
     {
-    "id": "video_03",
-    "topic": "Flutter",
-    "description": "video_03",
+    "id": "Componente y permisos de android",
+    "topic": "PSP",
+    "description": "Componente y permisos de android",
     "duration": 330,
     "thumbnail": "/assets/videos/video_03/thumbnail/thumbnail_video_03.png",
     "videoUrl": "/assets/videos/video_03/hls/index.m3u8"
